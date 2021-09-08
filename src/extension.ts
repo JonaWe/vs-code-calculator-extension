5ec7840471d777329b5bb7bb171ad765055c8303
Jona Wessendorf <jona.wessendorf1@uni-bielefeld.de>
2021-09-08 23:39:20 +0200

    adds evaluate command showing an input box

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,39 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { evaluate } from 'mathjs';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "calculator" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    'calculator.evaluate',
+    () => {
+      vscode.window
+        .showInputBox({
+          title: 'Calculator',
+          placeHolder: 'type an expression to evaluate',
+          prompt: 'Evaluate expression',
+        })
+        .then((input) => {
+          if (input) {
+            try {
+              const evaluated = evaluate(input);
+              vscode.window.showInformationMessage(
+                `Result of expression: ${evaluated}`
+              );
+            } catch {
+              vscode.window.showErrorMessage(
+                `ERROR: unable to evaluate '${input}'!`
+              );
+            }
+          }
+        });
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('calculator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Calculator!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
